bb274abee1231e9fd4fcd8276bc606ba5d183ba3
Javier Olaechea <pirata@gmail.com>
2013-06-18 17:10:05 -0400

    Pair Programming Session with Pedro/Javier

--- connectFour.py
+++ connectFour.py
@@ -1,70 +1,76 @@
 #!/usr/bin/env python
+"""
+Connect 4 for Python
+"""
+import sys
 
-#Connect 4 for Python
 
 class ConnectFour(object):
     def __init__(self, columns=7, column_size=6):
         self.board = Board(columns, column_size)
 
     def checkWinner(self):
-        return self.board.checkHorizontal() or self.board.checkVerticalDiagonal()
+        return (self.board.checkHorizontal() or
+                self.board.checkVerticalDiagonal())
 
-    def checkValidInput(self, userInput):
-        valid = False
-        try:
-            userInput = int(userInput)
-        except ValueError:
-            pass
-        else:
-            if userInput in range(1, self.board.columns + 1):
-                valid = True
-        if type(userInput) is str and userInput.lower() == 'q':
-            valid = True
-        return valid
+    def checkValidInput(self, user_input):
+        user_input = user_input.lower()
+        return (user_input in
+                [str(x) for x in range(1, self.board.columns + 1)] + ['q'])
+
+    def read_player_move(self, current_player):
+        while True:
+            move = raw_input(
+                current_player + "'s turn. Enter a column number to move: ")
+            if self.checkValidInput(move):
+                return move
 
     def play(self):
         player = 'P'
         counter = 1
-        while True:
-            self.board.drawBoard()
-            msg = player + '\'s turn. '
-            d = raw_input(msg + "Enter a column number to move: ")
-            if self.checkValidInput(d):
-                try:
-                    d = int(d)
-                except ValueError:
-                    pass
-                if type(d) is int:
-                    for index, row in enumerate(self.board.board):
-                        if self.board.checkEmpty(index, d-1):
-                            self.board.board[index][d-1] = player
-                            if player == 'P':
-                                player = 'C'
-                            elif player == 'C':
-                                player = 'P'
-                            break
-                else:
-                    if d.lower() == 'q':
-                        break
-            potentialWinner = self.checkWinner()
-            if potentialWinner:
-               print str(potentialWinner) + " won the game!"
-               self.board.drawBoard()
-               break
+        while not self.checkWinner():
+            print self.board
+            move = self.read_player_move(player)
+            if move in ('q', 'Q'):
+                sys.exit(0)
+            move = int(move)
+            for cell in self.board:
+                if cell.col == move - 1 and cell.is_empty():
+                    self.board.set_cell(cell, player)
+                    player = self.next_player(player)
+                    break
+
+        print self.board
+        print player, " won the game!"
+
+    def next_player(self, player):
+        if player == 'P':
+            return 'C'
+        elif player == 'C':
+            return 'P'
+
 
 class Board(object):
-    def __init__(self, columns = 7, column_size = 6):
+    def __init__(self, columns=7, column_size=6):
         self.columns = columns
         self.column_size = column_size
         self.resetBoard()
 
-    def checkEmpty(self, x, y):
-        return self.board[x][y] == '_'
+    def set_cell(self, cell, value):
+        self.board[cell.row][cell.col] = value
 
     def resetBoard(self):
-        self.board = [['_' for j in range(self.columns)] for i in range(self.column_size)]
+        self.board = [['_' for j in range(self.columns)]
+                      for i in range(self.column_size)]
 
-    def drawBoard(self):
+    def __iter__(self, ):
+        """
+        """
+        for row_index, row in enumerate(self.board):
+            for col_index, col in enumerate(row):
+                yield Cell(col, row_index, col_index)
+
+    def __str__(self):
         columnNumbers = ""
         for i in range(self.columns + 1):
             if i != 0:
@@ -98,29 +104,55 @@ class Board(object):
 
     def checkVerticalDiagonal(self):
         """
-        >>> 1 + 1
-        3
-
         """
         for rowIndex, row in enumerate(self.board):
             maxLength = len(self.board) - 4
             if rowIndex <= maxLength:
                 for index, val in enumerate(row):
                     if val == 'C' or val == 'P':
-                        # check vertical
-                        if val == self.board[rowIndex + 1][index] and val == self.board[rowIndex + 2][index] and val == self.board[rowIndex + 3][index]:
+                        if self.check_vertical_winning_condition(val,
+                                                                 rowIndex,
+                                                                 index):
                             return val
                         # check diagonal
-                        if (index <= maxLength and val == self.board[rowIndex + 1][index + 1] and val == self.board[rowIndex + 2][index + 2] and val == self.board[rowIndex + 3][index + 3]) or \
-                        (index >= maxLength and val == self.board[rowIndex + 1][index - 1] and val == self.board[rowIndex + 2][index - 2] and val == self.board[rowIndex + 3][index - 3]):
+                        if self.check_diagonal_winning_condition(val,
+                                                                 rowIndex,
+                                                                 index,
+                                                                 maxLength):
                             return val
         return False
 
+    def check_vertical_winning_condition(self, val, row_index, index):
+        return (val == self.board[row_index + 1][index] and
+                val == self.board[row_index + 2][index] and
+                val == self.board[row_index + 3][index])
+
+    def check_diagonal_winning_condition(self, val, row_index,
+                                         index, max_length):
+        return ((index <= max_length and
+                 val == self.board[row_index + 1][index + 1] and
+                 val == self.board[row_index + 2][index + 2] and
+                 val == self.board[row_index + 3][index + 3]) or
+                (index >= max_length and
+                 val == self.board[row_index + 1][index - 1] and
+                 val == self.board[row_index + 2][index - 2] and
+                 val == self.board[row_index + 3][index - 3]))
 
 
+class Cell(object):
+    def __init__(self, value, row_index, column_index):
+        self.value = value
+        self.row = row_index
+        self.col = column_index
+
+    def __str__(self):
+        return "{value} at ({row}, {col})".format(value=self.value,
+                                                  row=self.row,
+                                                  col=self.col)
+
+    def is_empty(self):
+        return self.value == '_'
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-    #game = ConnectFour()
-    #game.play()
+    game = ConnectFour()
+    game.play()
