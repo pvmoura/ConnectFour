c41fcbf65f779e7dbb4ec29c285c308224ddb295
Pedro Moura <pmavfmoura@gmail.com>
2013-06-24 16:35:45 -0400

    continuing to implement minimax

--- connectFour.py
+++ connectFour.py
@@ -4,11 +4,13 @@ Connect 4 for Python
 """
 import sys
 import random
+import copy
 
 
 class HumanPlayer(object):
     def __init__(self, turn=True, name='P', current_board=None):
         self.name = name
+
         self.turn = turn
         self.current_board = current_board
 
@@ -36,9 +38,9 @@ class ComputerPlayer(object):
         # add a move function for the computer
         self.current_board = board
         possible_moves = self.current_board.list_possible_moves()
-        possible_move = self.check_for_winning_move(board, possible_moves)
-        if possible_move:
-            return possible_move[1] + 1
+        move = self.check_for_immediate_win(board, possible_moves)
+        if move:
+            return move
         return possible_moves[random.randint(0, len(possible_moves) - 1)][1] + 1
 
     def evaluate_board_utility(self, board):
@@ -49,19 +51,57 @@ class ComputerPlayer(object):
             #if board.count_sets_of_adjacent_checkers()
         return True
 
-    def check_for_winning_move(self, board, possible_moves):
-        cell_list = list(board)
-        new_board = Board(cell_list)
-        for move in possible_moves:
-            new_board.board[move[0]][move[1]].value = self.opponent
-            if new_board.check_for_win():
-                return move
-            new_board.board[move[0]][move[1]].value = self.name
-            if new_board.check_for_win():
-                return move
-            new_board.board[move[0]][move[1]].value = '_'
-        return new_board.check_for_win()
 
+    def recurse_through_minimax(self, board):
+        if self.counter == 4 or len(board.list_possible_moves()) == 0:
+            return evaluate_board_utility(board)
+        else:
+            self.tree = max()
+            recurse_through_minimax(board)
+
+    def create_future_boards(self, board):
+        """This function creates future boards
+        """
+        new_board = copy.deepcopy(board)
+        new_board.list_possible_moves()
+
+    def choose_next_move(self, ):
+        """In this function I want to iterate through the possible_moves
+        and for each move create a branch of subsequent possible moves.
+        then, once I reach 4 levels deep or whatever I evaluate the terminal
+        state and minimax backwards
+        """
+        possible_moves = self.current_board.list_possible_moves()
+        # call branch creator that defines future possible moves
+
+
+    def create_possible_boards(self, board):
+        new_board = copy.deepcopy(board)
+
+    def create_list_of_possible_boards(self, ):
+        pass
+
+    def test_possible_moves(self, board, possible_moves, player_name,
+                            direction_list, check_value):
+        for move in possible_moves:
+            cell = board.get_cell(move)
+            for direction in direction_list:
+                cell.value = player_name
+                if (board.count_sets_of_adjacent_checkers(cell, direction[0]) + 
+                    board.count_sets_of_adjacent_checkers(cell, direction[1])) == check_value:
+                    cell.value = '_'
+                    return move
+                cell.value = '_'
+        return False
+
+    def check_for_immediate_win(self, board, possible_moves):
+        combination_directions = [('nw','se'), ('ne', 'sw'), ('e', 'w'), ('s', 'n')]
+        return_value = self.test_possible_moves(board, possible_moves,
+                                            self.name, combination_directions, 3)
+        if not return_value:
+            return_value = self.test_possible_moves(board, possible_moves,
+                                                self.opponent, combination_directions, 3)
+        return return_value
 
 
 class ConnectFour(object):
@@ -69,7 +109,16 @@ class ConnectFour(object):
         self.board = Board()
 
     def read_player_move(self, ):
-        return self.current_player.move(self.board)
+        move = self.current_player.move(self.board)
+        if type(move) is not tuple:
+            try:
+                move = int(move) - 1
+                possible_moves = self.board.list_possible_moves()
+                position = [elem for elem in possible_moves if elem[1] == move]
+                move = position[0]
+            except ValueError:
+                pass
+        return move
 
     def play(self, human=False):
         players = [HumanPlayer(), HumanPlayer(False, 'Q')]
@@ -81,10 +130,7 @@ class ConnectFour(object):
             move = self.read_player_move()
             if move in ('q', 'Q'):
                 sys.exit(0)
-            move = int(move) - 1
-            possible_moves = self.board.list_possible_moves()
-            position = [elem for elem in possible_moves if elem[1] == move]
-            self.board.set_cell(position[0], self.current_player.name)
+            self.board.set_cell(move, self.current_player.name)
             self.current_player = self.get_next_player(players)[0]
 
         print self.board
@@ -141,6 +187,13 @@ class Board(object):
 
     def create_board(self, cell_list):
         self.board = [cell_list[x:x+self.columns] for x in xrange(0, len(cell_list), self.columns)]
+
+    def copy_board(self, ):
+        cell_list = [Cell(cell.value, cell.row, cell.col) for cell in self]
+        return Board(cell_list)
+
+    def get_cell(self, position_tuple):
+        return self.board[position_tuple[0]][position_tuple[1]]
 
     def set_cell(self, position_tuple, player_name):
         cell = self.board[position_tuple[0]][position_tuple[1]]
